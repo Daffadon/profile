431d7f8d960d00bfabd97167aa94a5fa53cc4560
Daffadon <daffaputranarendra9@gmail.com>
2024-08-25 22:56:22 +0700

    refactor: change responsive size and published date format

--- components/card/Articlecard.tsx
+++ components/card/Articlecard.tsx
@@ -18,12 +18,12 @@ const Articlecard: FC<ArticleCardProps> = ({
   article: { id, title, link, author, published, created, category, content },
 }) => {
   const date = new Date(published);
-  const day = date.toUTCString();
+  const day = date.toLocaleString();
   return (
-    <Link href={link} target="_blank">
-      <div className="w-full border border-black px-3 py-2 rounded hover:scale-[99%] duration-300 max-h-[20vh] sm:max-h-[24vh] lg:min-h-[20vh] lg:max-h-[25vh]">
+    <Link href={link} target="_blank" className="">
+      <div className="w-full border border-black px-3 py-2 rounded hover:scale-[99%] duration-300 min-h-max max-h-[21vh] max-w-[30em] sm:min-h-[25vh] lg:min-h-[30vh] xl:min-h-[24vh] 2xl:min-h-[20vh] sm:max-w-none">
         <h2 className="font-bold">{title}</h2>
-        <p className="mb-3">{day}</p>
+        <p className="my-2 text-sm">Published: {day}</p>
         <div className="flex flex-wrap gap-1">
           {category.map((cat, index) => (
             <p key={index} className="bg-gray-300 px-2 py-1 text-xs rounded">
